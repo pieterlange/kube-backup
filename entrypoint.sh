bacc9e6f389eaf01b6d5b9b379914fd2042f1144
Pieter Lange <pieter@ptlc.nl>
2017-03-15 16:54:46 +0100

    jq syntax error

--- entrypoint.sh
+++ entrypoint.sh
@@ -29,7 +29,7 @@ for resource in $GLOBALRESOURCES; do
           .items[].metadata.selfLink,
           .items[].metadata.resourceVersion,
           .items[].metadata.creationTimestamp,
-          .items[].metadata.generation,
+          .items[].metadata.generation
       )' | python -c 'import sys, yaml, json; yaml.safe_dump(json.load(sys.stdin), sys.stdout, default_flow_style=False)' > /backup/git/${resource}.yaml
 done
 
