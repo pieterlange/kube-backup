e9a2d48e401e122cac757b6efb14a7c0fe3c72bf
Pieter Lange <pieter@ptlc.nl>
2017-04-20 16:23:54 +0200

    Upgrade kubectl to 1.6.2

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 set -ex
-KUBECTL_VERSION=1.5.4
+KUBECTL_VERSION=1.6.2
 
 get_bin()
 {
@@ -17,7 +17,7 @@ apk add --update bash easy-rsa git openssh-client curl ca-certificates jq python
 
 pip install ijson
 
-get_bin 1d9be6531f9830798b94d2a3ba6d07592ac664b8e12a8ddf201dd815719b9563 \
+get_bin 9beec3e8a9208da5cac479a164a61bf6a7b0b8716c338f866c4316680f0e9d98 \
     "https://storage.googleapis.com/kubernetes-release/release/v${KUBECTL_VERSION}/bin/linux/amd64/kubectl"
 
 adduser -h /backup -D backup
