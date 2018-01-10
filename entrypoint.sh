5ae8c682d4936f3e0f3912157d41717130df505e
kube-backup <kube-backup@example.com>
2018-01-10 11:24:47 -0700

    format with https://github.com/mvdan/sh using 4 spaces.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 if [ -z "$NAMESPACES" ]; then
-  NAMESPACES=$(kubectl get ns -o jsonpath={.items[*].metadata.name})
+    NAMESPACES=$(kubectl get ns -o jsonpath={.items[*].metadata.name})
 fi
 
 RESOURCETYPES="${RESOURCETYPES:-"ingress deployment configmap svc rc ds crd networkpolicy statefulset storageclass cronjob"}"
@@ -18,37 +18,37 @@ GITCRYPT_ENABLE="${GITCRYPT_ENABLE:-"false"}"
 GITCRYPT_PRIVATE_KEY="${GITCRYPT_PRIVATE_KEY:-"/secrets/gpg-private.key"}"
 GITCRYPT_SYMMETRIC_KEY="${GITCRYPT_SYMMETRIC_KEY:-"/secrets/symmetric.key"}"
 
-if [[ ! -f /backup/.ssh/id_rsa ]] ; then
+if [[ ! -f /backup/.ssh/id_rsa ]]; then
     git config --global credential.helper '!aws codecommit credential-helper $@'
     git config --global credential.UseHttpPath true
 fi
 [ -z "$DRY_RUN" ] && git config --global user.name "$GIT_USERNAME"
 [ -z "$DRY_RUN" ] && git config --global user.email "$GIT_EMAIL"
 
-[ -z "$DRY_RUN" ] && ( test -d "$GIT_REPO_PATH" || git clone --depth 1 "$GIT_REPO" "$GIT_REPO_PATH" --branch "$GIT_BRANCH" || git clone "$GIT_REPO" "$GIT_REPO_PATH" )
+[ -z "$DRY_RUN" ] && (test -d "$GIT_REPO_PATH" || git clone --depth 1 "$GIT_REPO" "$GIT_REPO_PATH" --branch "$GIT_BRANCH" || git clone "$GIT_REPO" "$GIT_REPO_PATH")
 cd "$GIT_REPO_PATH"
-[ -z "$DRY_RUN" ] && ( git checkout "${GIT_BRANCH}" || git checkout -b "${GIT_BRANCH}" )
+[ -z "$DRY_RUN" ] && (git checkout "${GIT_BRANCH}" || git checkout -b "${GIT_BRANCH}")
 [ -z "$DRY_RUN" ] && git stash
 if [ "$GITCRYPT_ENABLE" = "true" ]; then
-  if [ -f "$GITCRYPT_PRIVATE_KEY" ]; then
-    gpg --allow-secret-key-import --import "$GITCRYPT_PRIVATE_KEY"
-    git-crypt unlock
-  elif [ -f "$GITCRYPT_SYMMETRIC_KEY" ]; then
-    git-crypt unlock "$GITCRYPT_SYMMETRIC_KEY"
-  else
-    echo "[ERROR] Please verify your env variables (GITCRYPT_PRIVATE_KEY or GITCRYPT_SYMMETRIC_KEY)"
-    exit 1
-  fi
+    if [ -f "$GITCRYPT_PRIVATE_KEY" ]; then
+        gpg --allow-secret-key-import --import "$GITCRYPT_PRIVATE_KEY"
+        git-crypt unlock
+    elif [ -f "$GITCRYPT_SYMMETRIC_KEY" ]; then
+        git-crypt unlock "$GITCRYPT_SYMMETRIC_KEY"
+    else
+        echo "[ERROR] Please verify your env variables (GITCRYPT_PRIVATE_KEY or GITCRYPT_SYMMETRIC_KEY)"
+        exit 1
+    fi
 fi
 cd "$GIT_REPO_PATH/$GIT_PREFIX_PATH"
 git rm -r **/*.yaml || true
 
 # Start kubernetes state export
 for resource in $GLOBALRESOURCES; do
-  [ -d "$GIT_REPO_PATH/$GIT_PREFIX_PATH" ] || mkdir -p "$GIT_REPO_PATH/$GIT_PREFIX_PATH"
-  echo "Exporting resource: ${resource}" > /dev/stderr
-  kubectl get --export -o=json "$resource" | jq --sort-keys \
-      'del(
+    [ -d "$GIT_REPO_PATH/$GIT_PREFIX_PATH" ] || mkdir -p "$GIT_REPO_PATH/$GIT_PREFIX_PATH"
+    echo "Exporting resource: ${resource}" >/dev/stderr
+    kubectl get --export -o=json "$resource" | jq --sort-keys \
+        'del(
           .items[].metadata.annotations."kubectl.kubernetes.io/last-applied-configuration",
           .items[].metadata.annotations."control-plane.alpha.kubernetes.io/leader",
           .items[].metadata.uid,
@@ -56,24 +56,24 @@ for resource in $GLOBALRESOURCES; do
           .items[].metadata.resourceVersion,
           .items[].metadata.creationTimestamp,
           .items[].metadata.generation
-      )' | python -c 'import sys, yaml, json; yaml.safe_dump(json.load(sys.stdin), sys.stdout, default_flow_style=False)' > "$GIT_REPO_PATH/$GIT_PREFIX_PATH/${resource}.yaml"
+      )' | python -c 'import sys, yaml, json; yaml.safe_dump(json.load(sys.stdin), sys.stdout, default_flow_style=False)' >"$GIT_REPO_PATH/$GIT_PREFIX_PATH/${resource}.yaml"
 done
 
 for namespace in $NAMESPACES; do
-  [ -d "$GIT_REPO_PATH/$GIT_PREFIX_PATH/${namespace}" ] || mkdir -p "$GIT_REPO_PATH/$GIT_PREFIX_PATH/${namespace}"
+    [ -d "$GIT_REPO_PATH/$GIT_PREFIX_PATH/${namespace}" ] || mkdir -p "$GIT_REPO_PATH/$GIT_PREFIX_PATH/${namespace}"
 
-  for type in $RESOURCETYPES; do
-    echo "[${namespace}] Exporting resources: ${type}" > /dev/stderr
+    for type in $RESOURCETYPES; do
+        echo "[${namespace}] Exporting resources: ${type}" >/dev/stderr
 
-    label_selector=""
-    if [[ "$type" == 'configmap' && -z "${INCLUDE_TILLER_CONFIGMAPS:-}" ]]; then
-      label_selector="-l OWNER!=TILLER"
-    fi
+        label_selector=""
+        if [[ "$type" == 'configmap' && -z "${INCLUDE_TILLER_CONFIGMAPS:-}" ]]; then
+            label_selector="-l OWNER!=TILLER"
+        fi
 
-    kubectl --namespace="${namespace}" get "$type" $label_selector -o custom-columns=SPACE:.metadata.namespace,KIND:..kind,NAME:.metadata.name --no-headers | while read -r a b name; do
+        kubectl --namespace="${namespace}" get "$type" $label_selector -o custom-columns=SPACE:.metadata.namespace,KIND:..kind,NAME:.metadata.name --no-headers | while read -r a b name; do
 
-    kubectl --namespace="${namespace}" get --export -o=json "$type" "$name" | jq --sort-keys \
-        'select(.type!="kubernetes.io/service-account-token") |
+            kubectl --namespace="${namespace}" get --export -o=json "$type" "$name" | jq --sort-keys \
+                'select(.type!="kubernetes.io/service-account-token") |
         del(
             .metadata.annotations."control-plane.alpha.kubernetes.io/leader",
             .metadata.annotations."kubectl.kubernetes.io/last-applied-configuration",
@@ -84,16 +84,16 @@ for namespace in $NAMESPACES; do
             .metadata.uid,
             .spec.clusterIP,
             .status
-        )' | python -c 'import sys, yaml, json; yaml.safe_dump(json.load(sys.stdin), sys.stdout, default_flow_style=False)' > "$GIT_REPO_PATH/$GIT_PREFIX_PATH/${namespace}/${name}.${type}.yaml"
-  done
-  done
+        )' | python -c 'import sys, yaml, json; yaml.safe_dump(json.load(sys.stdin), sys.stdout, default_flow_style=False)' >"$GIT_REPO_PATH/$GIT_PREFIX_PATH/${namespace}/${name}.${type}.yaml"
+        done
+    done
 done
 
 [ -z "$DRY_RUN" ] || exit
 
 git add .
 
-if ! git diff-index --quiet HEAD -- ; then
+if ! git diff-index --quiet HEAD --; then
     git commit -m "Automatic backup at $(date)"
     git push origin "${GIT_BRANCH}"
 else
